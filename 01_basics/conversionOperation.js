3c1f1d06f86a59e0e6105cb8987789d6909bcaba
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-03-22 21:12:55 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,29 @@
+
+let score = "33abnh"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+//  "33" => 33
+// "33abc" => NaN
+// true =>1; false => 0
+
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true, 0 => false
+// "" => false
+//  "hitesh" => true
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
