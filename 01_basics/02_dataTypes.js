8b3803a35978626e29de252e4200cc6f023ae0e6
keshavAgarwal11 <73520882+keshavAgarwal11@users.noreply.github.com>
2025-03-22 19:18:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  //treat all js code as newer version
+// alert(3+3)  // we are using nodejs,not browser
+
+console.log(3
+  +
+3)    //code readability to be high
+console.log("keshav")
+
+let name = "keshava"
+let age = 18
+let isLogged = false
+let state;   //undefined data typr
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+ // symbol => unique
+
+
+
+
+
+//object
+
+
+// console.log(typeof "keshav")
+//  console.log(typeof age)
+//  console.log(typeof null) //object
+console.log(typeof undefined) //undefinrd
